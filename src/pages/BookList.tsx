890c1c05f493d8848bdbae2a883969989441306c
KshitijV97 <writetokshitij@gmail.com>
2023-06-27 15:45:57 +0530

    Simple component

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -1,13 +1,7 @@
 import { Link } from "react-router-dom";
 
 export const BookList = () => {
-	return (
-		<div>
-			<h1>BookList</h1>
-			<Link to='/book/1'>Book 1</Link>
-			<Link to='/book/2'>Book 2</Link>
-		</div>
-	);
+	return <h1>BookList</h1>;
 };
 
 export default BookList;
