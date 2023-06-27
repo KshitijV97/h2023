5f5a2d9d32558e8e65e751b31b18beeb1e0296af
KshitijV97 <writetokshitij@gmail.com>
2023-06-27 20:38:10 +0530

    Added zustand example and showing nav only on root

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import "./App.css";
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { BookRoutes } from "./routes/BookRoutes";
+import { ZustandExample } from "./pages/ZustandExample/ZustandExample";
 
 function App() {
+	const location = useLocation();
+
+	const showNav: boolean = location.pathname === "/";
 	return (
 		<div>
-			<nav>
-				<ul>
-					<li>
-						<Link to='/'>Home</Link>
-					</li>
-					<li>
-						<Link to='/books'>Books</Link>
-					</li>
-				</ul>
-			</nav>
+			{showNav ? (
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/books'>Books</Link>
+						</li>
+						<li>
+							<Link to='/zustand-example'>Zustand example</Link>
+						</li>
+					</ul>
+				</nav>
+			) : null}
+
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/books/*' element={<BookRoutes />} />
+				<Route path='/zustand-example' element={<ZustandExample />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
 		</div>
