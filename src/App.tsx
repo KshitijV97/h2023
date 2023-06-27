96a8470dfef0896d369fce1e4d2ae5f28d203bcb
KshitijV97 <writetokshitij@gmail.com>
2023-06-27 07:41:10 +0530

    Added react router with params and not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-function App() {
-	console.log("Hello world");
+import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Book } from "./pages/Book";
+import { BookList } from "./pages/BookList";
+import { NotFound } from "./pages/NotFound";
+import NewBook from "./pages/NewBook";
 
-	return <div>Team Synthix</div>;
+function App() {
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline text-center'>Hello world!</h1>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/books'>Books</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/books' element={<BookList />} />
+				<Route path='/book/:id' element={<Book />} />
+				<Route path='/book/new' element={<NewBook />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
