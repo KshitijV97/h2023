ffaef8a816c735af705692279b98f43af222152d
Yash Hemant Patil <yash-hemant.patil@breakingwave.io>
2023-06-28 21:01:47 +0530

    PaLM2 call to VertexAI

--- nlp-python/main.py
+++ nlp-python/main.py
@@ -5,10 +5,21 @@
 
 import vertexai
 from vertexai.language_models import TextGenerationModel
+import google.auth
 
-def parts_of_speech():
+import os
 
-    vertexai.init(project="ac-cntxtlbank-sct-project", location="us-central1", credentials="ya29.a0AWY7Ckl6UMaP9Zn94otPrQ10ZIpdeV8C5MVyDS5NBJyk08tglL-fPR_33yxw9yGQ3tflPKTAhPf4q8j8HHNvMR5IssqvR8IndWs8UCMLiGWmJwOfOAvH10DBqzL0O7AgWAY82QGIKMoj-C4XPrQWoQBK_wU3mzr7TObODpcaCgYKAS0SARISFQG1tDrpPKiP9mBcPE2LZwXOM_ZKhA0174")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.expanduser('~/.config/gcloud/application_default_credentials.json')
+
+def parts_of_speech(input_to_llm):
+    credentials = google.auth.load_credentials_from_file(
+        os.path.expanduser('~/.config/gcloud/application_default_credentials.json')
+    )
+    vertexai.init(
+        project="ac-cntxtlbank-sct-project",
+        location="us-central1",
+        # credentials=credentials
+    )
     parameters = {
         "temperature": 0.2,
         "max_output_tokens": 256,
@@ -16,13 +27,14 @@ def parts_of_speech():
         "top_k": 40
     }
     model = TextGenerationModel.from_pretrained("text-bison@001")
+
     response = model.predict(
-        """Extract various parts of the given text into items like email, address, phone number. 
-    text: Hi, My name is Yash Patil, i work with Google Cloud Platform and live in Privet Drive, Surrey. You can contact me at 9876543210
-    """,
+        input_to_llm,
         **parameters
     )
-    print(f"Response from Model: {response.text}")
+    print(f"Response from Model: \n")
+    print(response.text)
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -31,7 +43,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    parts_of_speech()
+    input_to_llm = """Extract various parts of the given text into items like email, address, phone number. 
+        text: Hi, My name is Yash Patil, i work with Google Cloud Platform and live in Privet Drive, Surrey. You can contact me at 9876543210
+        """
+    parts_of_speech(input_to_llm)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
