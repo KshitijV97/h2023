8f7e9bfc9d75f89f0b7270bdfe86521b67172e60
KshitijV97 <writetokshitij@gmail.com>
2023-06-29 09:05:44 +0530

    Refactored to TypeScript

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -1,6 +1,12 @@
-import axios from "axios";
+import axios, {
+	AxiosInstance,
+	InternalAxiosRequestConfig,
+	AxiosRequestConfig,
+	AxiosResponse,
+	AxiosError,
+} from "axios";
 
-export const axiosInstance = axios.create({
+export const axiosInstance: AxiosInstance = axios.create({
 	baseURL: "",
 	timeout: 5000,
 	headers: {
@@ -12,11 +18,11 @@ export const axiosInstance = axios.create({
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
-	function (config) {
+	function (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
 		// Do something before request is sent
 		return config;
 	},
-	function (error) {
+	function (error: AxiosError): Promise<AxiosError> {
 		// Do something with request error
 		return Promise.reject(error);
 	}
@@ -24,12 +30,12 @@ axiosInstance.interceptors.request.use(
 
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
-	function (response) {
+	function (response: AxiosResponse): AxiosResponse {
 		// Any status code that lie within the range of 2xx cause this function to trigger
 		// Do something with response data
 		return response;
 	},
-	function (error) {
+	function (error: AxiosError): Promise<AxiosError> {
 		// Any status codes that falls outside the range of 2xx cause this function to trigger
 		// Do something with response error
 		return Promise.reject(error);
