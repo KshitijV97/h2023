bb41008ee24ad76e102a4752b509ca0359a5d8d2
KshitijV97 <writetokshitij@gmail.com>
2023-06-29 09:05:16 +0530

    Added mock service worker

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
+import { setupWorker } from "msw";
+import { handlers } from "./mocks/handlers";
+
+// This configures a Service Worker with the given request handlers.
+export const worker = setupWorker(...handlers);
+
+if (process.env.NODE_ENV === "development") {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
