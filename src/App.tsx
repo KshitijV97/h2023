7f5185c6a7016db89486db442d1db8b39c113021
bw-prakhar-sankrityayan <prakhar.sankrityayan@synthix.com>
2023-07-09 17:34:50 +0530

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,54 @@
 import "./App.css";
-import { Link, Routes, Route, useLocation } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { BookRoutes } from "./routes/BookRoutes";
 import { ZustandExample } from "./pages/ZustandExample/ZustandExample";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme/theme";
 import SignInSide from "./pages/SignInSide/SignInSide";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import Dashboard from "./pages/DashboardNew/Dashboard";
+import Sidebar from "./pages/global/Sidebar";
+import Topbar from "./pages/global/Topbar";
+import { useState } from "react";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
-	const location = useLocation();
-
-	const showNav: boolean = location.pathname === "/";
-	return (
-		<ThemeProvider theme={theme}>
-			<div>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				{showNav ? (
-					<nav>
-						<ul>
-							<li>
-								<Link to='/'>Home</Link>
-							</li>
-							<li>
-								<Link to='/books'>Books</Link>
-							</li>
-							<li>
-								<Link to='/zustand-example'>Zustand example</Link>
-							</li>
-						</ul>
-					</nav>
-				) : null}
-
-				<Routes>
-					<Route path='/' element={<SignInSide />} />
-					<Route path='/dashboard' element={<Dashboard />} />
-					<Route path='/books/*' element={<BookRoutes />} />
-					<Route path='/zustand-example' element={<ZustandExample />} />
-					<Route path='*' element={<NotFound />} />
-				</Routes>
-			</div>
-		</ThemeProvider>
-	);
+  const [theme, colorMode] = useMode();
+  const location = useLocation();
+  const [isSidebar, setIsSidebar] = useState(true);
+  console.log("this is setIsSidebar", isSidebar);
+  //   const showNav: boolean = location.pathname === "/";
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<SignInSide />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/books" element={<BookRoutes />} />
+              <Route path="/zustand-example" element={<ZustandExample />} />
+              <Route path="*" element={<NotFound />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -23,119 +23,119 @@ import { Drawer } from "@mui/material";
 import { AppBar } from "../../components/AppBar/AppBar";
 
 export default function Dashboard() {
-	const [open, setOpen] = React.useState(true);
-	const toggleDrawer = () => {
-		setOpen(!open);
-	};
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<CssBaseline />
-			<AppBar position='absolute' open={open}>
-				<Toolbar
-					sx={{
-						pr: "24px", // keep right padding when drawer closed
-					}}
-				>
-					<IconButton
-						edge='start'
-						color='inherit'
-						aria-label='open drawer'
-						onClick={toggleDrawer}
-						sx={{
-							marginRight: "36px",
-							...(open && { display: "none" }),
-						}}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						component='h1'
-						variant='h6'
-						color='inherit'
-						noWrap
-						sx={{ flexGrow: 1 }}
-					>
-						Dashboard
-					</Typography>
-					<IconButton color='inherit'>
-						<Badge badgeContent={4} color='secondary'>
-							<NotificationsIcon />
-						</Badge>
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<Drawer variant='permanent' open={open}>
-				<Toolbar
-					sx={{
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "flex-end",
-						px: [1],
-					}}
-				>
-					<IconButton onClick={toggleDrawer}>
-						<ChevronLeftIcon />
-					</IconButton>
-				</Toolbar>
-				<Divider />
-				<List component='nav'>
-					{mainListItems}
-					<Divider sx={{ my: 1 }} />
-					{secondaryListItems}
-				</List>
-			</Drawer>
-			<Box
-				component='main'
-				sx={{
-					backgroundColor: (theme) =>
-						theme.palette.mode === "light"
-							? theme.palette.grey[100]
-							: theme.palette.grey[900],
-					flexGrow: 1,
-					height: "100vh",
-					overflow: "auto",
-				}}
-			>
-				<Toolbar />
-				<Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-					<Grid container spacing={3}>
-						{/* Chart */}
-						<Grid item xs={12} md={8} lg={9}>
-							<Paper
-								sx={{
-									p: 2,
-									display: "flex",
-									flexDirection: "column",
-									height: 240,
-								}}
-							>
-								<Chart />
-							</Paper>
-						</Grid>
-						{/* Recent Deposits */}
-						<Grid item xs={12} md={4} lg={3}>
-							<Paper
-								sx={{
-									p: 2,
-									display: "flex",
-									flexDirection: "column",
-									height: 240,
-								}}
-							>
-								<Deposits />
-							</Paper>
-						</Grid>
-						{/* Recent Orders */}
-						<Grid item xs={12}>
-							<Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-								<Orders />
-							</Paper>
-						</Grid>
-					</Grid>
-					<Copyright sx={{ pt: 4 }} />
-				</Container>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: "24px", // keep right padding when drawer closed
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          {mainListItems}
+          <Divider sx={{ my: 1 }} />
+          {secondaryListItems}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 240,
+                }}
+              >
+                <Chart />
+              </Paper>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={4} lg={3}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 240,
+                }}
+              >
+                <Deposits />
+              </Paper>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12}>
+              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                <Orders />
+              </Paper>
+            </Grid>
+          </Grid>
+          <Copyright sx={{ pt: 4 }} />
+        </Container>
+      </Box>
+    </Box>
+  );
 }
